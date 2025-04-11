a464e72c798465dd30e95fee159c97571edec53f
8octa <tataruoctavian5@gmail.com>
2025-04-11 10:36:49 +0300

    Update metronome

--- src/Metronome.jsx
+++ src/Metronome.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Howl } from "howler";
-import Draggable from "react-draggable";
 
 const Metronome = () => {
   const [bpm, setBpm] = useState(120);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [isDraggingSlider, setIsDraggingSlider] = useState(false);
   const [intervalMultiplier, setIntervalMultiplier] = useState(1);
-  multiplier;
   const [interval, setIntervalValue] = useState(60000 / bpm);
   const intervalRef = useRef(null);
 
@@ -77,58 +74,50 @@ const Metronome = () => {
   };
 
   return (
-    <Draggable
-      onDrag={(e, data) => {
-        if (isDraggingSlider) {
-          return false;
-        }
-      }}
-    >
-      <section className="bg-linear-to-r/longer from-gray-900 to-gray-800 flex flex-col justify-center w-[20rem] h-[20rem] p-10 m-5 shadow-2xl rounded-2xl cursor-default">
-        <div className="flex flex-col justify-center">
-          <div className="text-5xl font-bold flex justify-between items-center text-white">
-            <span>{bpm}</span>
-            <span>BPM</span>
-          </div>
-          <div className="my-5">
-            <input
-              className="cursor-pointer text-red w-full bg-gray-200"
-              type="range"
-              min="40"
-              max="240"
-              value={bpm}
-              onChange={handleBpmChange}
-              onMouseDown={() => setIsDraggingSlider(true)}
-              onMouseUp={() => setIsDraggingSlider(false)}
-              onMouseLeave={() => setIsDraggingSlider(false)}
-              onTouchStart={() => setIsDraggingSlider(true)}
-              onTouchEnd={() => setIsDraggingSlider(false)}
-            />
-          </div>
-          <div className="my-5">
-            <select
-              className="cursor-pointer text-red w-full bg-gray-800 text-xl"
-              value={intervalMultiplier}
-              onChange={handleIntervalChange}
-            >
-              {intervals.map((option, index) => (
-                <option className="font-xl" key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="flex justify-center">
-            <button
-              onClick={togglePlay}
-              className="cursor-pointer focus:outline-none"
-            >
-              {isPlaying ? <StopButton /> : <PlayButton />}
-            </button>
-          </div>
+    <section className="bg-linear-to-r/longer from-gray-900 to-gray-800 flex flex-col justify-center w-[20rem] h-[20rem] p-10 m-5 shadow-2xl rounded-2xl cursor-default">
+      <div className="flex flex-col justify-center">
+        <div className="text-5xl font-bold flex justify-between items-center text-white">
+          <span>{bpm}</span>
+          <span>BPM</span>
         </div>
-      </section>
-    </Draggable>
+        <div className="my-5">
+          <input
+            className="cursor-pointer text-red w-full bg-gray-200"
+            type="range"
+            min="40"
+            max="240"
+            value={bpm}
+            onChange={handleBpmChange}
+            onMouseDown={() => setIsDraggingSlider(true)}
+            onMouseUp={() => setIsDraggingSlider(false)}
+            onMouseLeave={() => setIsDraggingSlider(false)}
+            onTouchStart={() => setIsDraggingSlider(true)}
+            onTouchEnd={() => setIsDraggingSlider(false)}
+          />
+        </div>
+        <div className="my-5">
+          <select
+            className="cursor-pointer text-red w-full bg-gray-800 text-xl"
+            value={intervalMultiplier}
+            onChange={handleIntervalChange}
+          >
+            {intervals.map((option, index) => (
+              <option className="font-xl" key={index} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={togglePlay}
+            className="cursor-pointer focus:outline-none"
+          >
+            {isPlaying ? <StopButton /> : <PlayButton />}
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
